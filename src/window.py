0e577fb441cdcf3b98111cd7c34ccf61f192cafe
Hartske <keatoru@gmail.com>
2025-05-11 13:46:29 -0700

    added comments and adjusted graphical display

--- src/window.py
+++ src/window.py
@@ -14,27 +14,31 @@ class Window():
         self.overlap = StringVar(value='False')
         self.error = StringVar()
 
+        # Make entry frame container and call build method
         self._entry_frame = ttk.Frame(
             self._root, borderwidth=2, relief='solid'
         )
-        self._entry_frame.grid(column=0, row=0, padx=5, pady=5)
+        self._entry_frame.grid(column=0, row=0, ipadx=15, ipady=15, padx=25, sticky='e')
         self._build_entry_frame()
 
+        # Make result frame container and call build method
         self._result_frame = ttk.Frame(
             self._root, borderwidth=2, relief='solid'
         )
-        self._result_frame.grid(column=1, row=0)
+        self._result_frame.grid(column=1, row=0, ipadx=15, ipady=15, padx=25, sticky='w')
         self._build_result_frame()
 
+        # Make display frame and call build method
         self._display_frame = ttk.Frame(
             self._root,).grid(column=0, row=1, columnspan=2, sticky='we'
         )
         self._build_display_frame()
 
+        # Configure layout
         self._root.columnconfigure(0, weight=1)
         self._root.columnconfigure(1, weight=1)
         self._root.rowconfigure(0, weight=1)
-        self._root.rowconfigure(1, weight=1)
+        self._root.rowconfigure(1, weight=2)
         # Focus and run
         self.run()
 
@@ -49,7 +53,7 @@ class Window():
             column=0, row=0, sticky='e'
         )
 
-        # Entry window for length of column or row
+        # Entry field for length of column or row
         self.length_entry = ttk.Entry(
             self._entry_frame, width=8, textvariable=self.length, font=('Arial', 20)
         )
@@ -60,40 +64,52 @@ class Window():
             self._entry_frame,
             text='List numbers in column or row. ex: 2,10,1,2', font=('Arial', 20)
         ).grid(
-            column=0, row=1, columnspan=2, sticky='we'
+            column=0, row=1, columnspan=2, padx=20
         )
 
-        # Entry window for list of numbers
+        # Entry field for list of numbers
         lst_entry = ttk.Entry(
             self._entry_frame, width=24, textvariable=self.lst, font=('Arial', 20)
         )
-        lst_entry.grid(column=0, row=2, columnspan=2, sticky='we', padx=10)
+        lst_entry.grid(column=0, row=2, columnspan=2, sticky='we', padx=20)
 
+        # Create button
         ttk.Button(
             self._entry_frame,
             text='Find Remainder', command=self._find_remainder
         ).grid(column=0, row=3, columnspan=2)
 
         self.length_entry.focus()
+        # Configure entry frame
+        self._entry_frame.columnconfigure(0, weight=1)
+        self._entry_frame.columnconfigure(1, weight=1)
+        self._entry_frame.rowconfigure(0, weight=1)
+        self._entry_frame.rowconfigure(1, weight=1)
+        self._entry_frame.rowconfigure(2, weight=1)
+        self._entry_frame.rowconfigure(3, weight=1)
 
     def _build_result_frame(self):
         rem = self.remainder.get()
+
+        # Remaining label and value
         ttk.Label(
-            self._result_frame, text='Remaining squares in row/column: ', font=('Arial', 20)
-        ).grid(column=0, row=0, sticky='e', padx=5)
+            self._result_frame, text='Remaining squares: ', font=('Arial', 20)
+        ).grid(column=0, row=0, sticky='e')
 
         label_remainder = ttk.Label(
             self._result_frame, textvariable=self.remainder, font=('Arial', 20)
-        ).grid(column=1, row=0, sticky='w', padx=5)
+        ).grid(column=1, row=0, sticky='w')
 
+        # Overlapping squares and value
         ttk.Label(
-            self._result_frame, text='Overlap?: ', font=('Arial', 20, )
-        ).grid(column=0,row=1, sticky='e', padx=5)
+            self._result_frame, text='Overlapping squares?: ', font=('Arial', 20, )
+        ).grid(column=0,row=1, sticky='e')
 
         label_overlap = ttk.Label(
             self._result_frame, textvariable=self.overlap, font=('Arial', 20)
-        ).grid(column=1, row=1, sticky='w', padx=5)
+        ).grid(column=1, row=1, sticky='w')
 
+        # Set error style and make label
         error_style = Style()
         error_style.configure('Error.TLabel', foreground='red')
         self.error_label = ttk.Label(
@@ -101,19 +117,31 @@ class Window():
             textvariable=self.error, font=('Arial', 20), style='Error.TLabel'
         ).grid(column=0, row=2, columnspan=2)
 
+        # Configure result frame
+        self._result_frame.columnconfigure(0, weight=1)
+        self._result_frame.columnconfigure(1, weight=1)
+        self._result_frame.rowconfigure(0, weight=1)
+        self._result_frame.rowconfigure(1, weight=1)
+        self._result_frame.rowconfigure(2, weight=1)
+
     def _build_display_frame(self):
-        self.horizontal = Canvas(self._display_frame, width=200, height=100)
-        self.horizontal.grid(column=0, row=1, columnspan=2, sticky='we')
+        # Make canvas
+        self.horizontal = Canvas(self._display_frame, width=200, height=200)
+        self.horizontal.grid(column=0, row=1, columnspan=2, sticky='nwe')
     
     def _find_remainder(self, *args):
+        # Try/Except check for inputs are integers
         try:
+            # Get list, split string at ',' remove white space and convert to integers
             _lst = self.lst.get()
             self.int_list = [int(num.strip()) for num in _lst.split(',')]
-            if sum(self.int_list) > int(self.length.get()):
+            # Chek if remainder is less than 0
+            rem = find_remainder(int(self.length.get()), self.int_list)
+            if  rem < 0:
                 self.error.set('Sum of numbers is greater than the length')
             else:
+                # Run calculations and draw grid
                 self.error.set('')
-                rem = find_remainder(int(self.length.get()), self.int_list)
                 self.remainder.set(str(rem))
                 overlap = check_overlap(self.int_list, rem)
                 self.overlap.set(overlap)
@@ -151,8 +179,8 @@ class Window():
                     e.x1, e.y1, e.x2, e.y2, fill='white', outline='black', width=2
                 )
                 self.horizontal.create_text(
-                    (e.x1 + e.x2) // 2, (e.y1 + e.y2) // 2 + square_size,
-                    text=str(i), font=('Arial', 20)
+                    (e.x1 + e.x2) // 2, (e.y1 + e.y2) // 2.5 + square_size,
+                    text=str(i), font=('Arial', 20, 'bold')
                 )
                 i += 1
             else:
@@ -160,7 +188,7 @@ class Window():
                     e.x1, e.y1, e.x2, e.y2, fill='black', outline='white', width=2
                 )
                 self.horizontal.create_text(
-                    (e.x1 + e.x2) // 2, (e.y1 + e.y2) // 2 + square_size,
-                    text=str(i), font=('Arial', 20)
+                    (e.x1 + e.x2) // 2, (e.y1 + e.y2) // 2.5 + square_size,
+                    text=str(i), font=('Arial', 20, 'bold')
                 )
                 i += 1
